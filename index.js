6fd4ab324070e09844a151dbf4e59755de62b1cd
sofiden <107278404+sofiden@users.noreply.github.com>
2022-06-18 20:36:26 +0300

    Add files via upload
    Commit changes

---
+++ index.js
@@ -0,0 +1,32 @@
+window.addEventListener('load', function () {
+    const videoIntro = document.getElementById('video-intro')
+    const videoIntro2 = document.getElementById('video-loop')
+    const introText = document.getElementById('intro-text')
+    const headerLogo = document.getElementById('header-logo')
+    const menu = document.getElementById('menu')
+    const text = document.getElementById('text')
+
+    videoIntro.addEventListener('ended', function () {
+
+        videoIntro.style.display = 'none'
+
+        videoIntro2.style.display = 'block'
+        videoIntro2.currentTime = 0        
+        videoIntro2.play()
+
+        introText.classList.add("lineUp");
+        introText.style.display = 'block'
+
+        headerLogo.classList.add("lineUp");
+        headerLogo.style.display = 'block'
+
+        menu.classList.add("lineUp");
+        menu.style.display = 'block'
+
+        text.classList.add("lineUp");
+        text.style.display = 'block'
+    })
+
+    console.log('load')
+})
+
